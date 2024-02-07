4ed9d4a72b441a8793a833df86b5875d3baca77c
Carolina Haggerty <carolina.vhaggerty@gmail.com>
2024-02-07 13:05:52 -0800

    The most significant changes involve the addition of new files for the Operator Lifecycle Manager (OLM) resources. These files include classes for various OLM resources such as `APIServiceDefinitions`, `Cleanup`, `GroupVersionKind`, `Icon`, `InstallModes`, `Links`, `Maintainers`, `NamedInstallStrategy`, `PolicyRule`, `Provider`, `RelatedImages`, `StrategyDeploymentPermissions`, `StrategyDeploymentSpec`, `StrategyDetailsDeployment`, `V1ClusterServiceVersion`, `V1ClusterServiceVersionSpec`, and `V1ClusterServiceVersionStatus`. Each class represents a specific resource type in the OLM and includes properties that correspond to the fields of the resource type.
    Additionally, two new C# files have been added: `OperatorDisplayNameAttribute.cs` and `OperatorNameAttribute.cs`. Both files are part of the `Neon.Kubernetes.Resources.OperatorLifecycleManager` namespace and define sealed classes with properties and constructors.
    
    Changes:
    
    1. Addition of new files for the Operator Lifecycle Manager (OLM) resources. These files include classes for various OLM resources.
    2. Each class represents a specific resource type in the OLM and includes properties that correspond to the fields of the resource type.
    3. Addition of two new C# files: `OperatorDisplayNameAttribute.cs` and `OperatorNameAttribute.cs`. Both files define sealed classes with properties and constructors.
    4. The `V1ClusterServiceVersion` class is decorated with the `KubernetesEntity` attribute and implements the `IKubernetesObject<V1ObjectMeta>`, `ISpec<V1ClusterServiceVersionSpec>`, and `IStatus<V1ClusterServiceVersionStatus>` interfaces.
    5. The `V1ClusterServiceVersionSpec` class includes properties that represent the specification of a ClusterServiceVersion resource in OLM.
    6. The `V1ClusterServiceVersionStatus` class is currently empty, indicating that it does not include any additional status information beyond what is included in the base `IKubernetesObjectStatus` interface.
    7. The `NamedInstallStrategy`, `StrategyDetailsDeployment`, `StrategyDeploymentPermissions`, and `StrategyDeploymentSpec` classes represent different aspects of the installation strategy for an operator.
    8. The `PolicyRule` class represents a policy rule for RBAC in Kubernetes.
    9. The `Provider` class represents the entity that publishes the operator.
    10. The `RelatedImages` class represents related images for the operator.
    11. The `Icon`, `Links`, `Maintainers`, `InstallModes`, `GroupVersionKind`, `APIServiceDefinitions`, and `Cleanup` classes represent various other aspects of an operator's metadata and configuration.
    12. `OperatorDisplayNameAttribute.cs` defines a sealed class `OperatorDisplayNameAttribute` with a single property `DisplayName`.
    13. `OperatorNameAttribute.cs` defines a sealed class `OperatorNameAttribute` that inherits from `Attribute`. This class has a single property `Name` and is decorated with the `AttributeUsage` attribute.

---
+++ src/Neon.Kubernetes.Resources/OperatorLifecycleManager/V1ClusterServiceVersion.cs
@@ -0,0 +1,98 @@
+// -----------------------------------------------------------------------------
+// FILE:	    V1ClusterServiceVersion.cs
+// CONTRIBUTOR: NEONFORGE Team
+// COPYRIGHT:   Copyright © 2005-2024 by NEONFORGE LLC.  All rights reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License").
+// You may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+using k8s.Models;
+using k8s;
+using Neon.K8s.Resources.Prometheus;
+using Neon.Operator.Attributes;
+
+namespace Neon.Kubernetes.Resources.OperatorLifecycleManager
+{
+    /// <summary>
+    /// ClusterServiceVersion.
+    /// </summary>
+    [KubernetesEntity(Group = KubeGroup, Kind = KubeKind, ApiVersion = KubeApiVersion, PluralName = KubePlural)]
+    [Ignore]
+    public class V1ClusterServiceVersion : IKubernetesObject<V1ObjectMeta>, ISpec<V1ClusterServiceVersionSpec>, IStatus<V1ClusterServiceVersionStatus>
+    {
+        
+        /// <summary>
+        /// The API version this Kubernetes type belongs to.
+        /// </summary>
+        public const string KubeApiVersion = "v1";
+
+        /// <summary>
+        /// The Kubernetes named schema this object is based on.
+        /// </summary>
+        public const string KubeKind = "ClusterServiceVersion";
+
+        /// <summary>
+        /// The Group this Kubernetes type belongs to.
+        /// </summary>
+        public const string KubeGroup = "monitoring.coreos.com";
+
+        /// <summary>
+        /// The plural name of the entity.
+        /// </summary>
+        public const string KubePlural = "clusterserviceversions";
+
+        /// <summary>
+        /// Initializes a new instance of the V1ClusterServiceVersion class.
+        /// </summary>
+        public V1ClusterServiceVersion()
+        {
+            ApiVersion = $"{KubeGroup}/{KubeApiVersion}";
+            Kind = KubeKind;
+        }
+
+        /// <summary>
+        /// Gets or sets APIVersion defines the versioned schema of this
+        /// representation of an object. Servers should convert recognized
+        /// schemas to the latest internal value, and may reject unrecognized
+        /// values. More info:
+        /// https://git.k8s.io/community/contributors/devel/sig-architecture/api-conventions.md#resources
+        /// </summary>
+        public string ApiVersion { get; set; }
+
+        /// <summary>
+        /// Gets or sets kind is a string value representing the REST resource
+        /// this object represents. Servers may infer this from the endpoint
+        /// the client submits requests to. Cannot be updated. In CamelCase.
+        /// More info:
+        /// https://git.k8s.io/community/contributors/devel/sig-architecture/api-conventions.md#types-kinds
+        /// </summary>
+        public string Kind { get; set; }
+
+        /// <summary>
+        /// Gets or sets standard object metadata.
+        /// </summary>
+        public V1ObjectMeta Metadata { get; set; }
+
+        /// <summary>
+        /// Gets or sets specification of the desired behavior of the
+        /// ServiceMonitor.
+        /// </summary>
+        public V1ClusterServiceVersionSpec Spec { get; set; }
+
+        public V1ClusterServiceVersionStatus Status { get; set; }
+
+    }
+}
