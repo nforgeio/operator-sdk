43b5f1166589bfb6b709ca5ecc188cec665120eb
Marcus Bowyer <marcus@euarctos.com>
2024-03-15 03:03:30 -0700

    Add V3ExampleEntity class and TestIgnoreAttributeCrd test method
    Added a new class `V3ExampleEntity` in the `Test_Analyzers` namespace with several constants, properties, nested classes, and interfaces. This class is decorated with various attributes and implements `IKubernetesObject<V3ObjectMeta>`, `ISpec<V3ExampleEntity.V3ExampleSpec>`, and `IStatus<V3ExampleEntity.V3ExampleStatus>` interfaces. The nested classes and properties are also decorated with attributes. Two enums `Gender` and `NullEnum` and an interface `IPerson` have been added to the `V3ExampleEntity` class. Additionally, a new test method `TestIgnoreAttributeCrd` has been added to the `Test.Analyzers` namespace in the `Test_Crds.cs` file. This test method builds a `TestCompilationBuilder` instance and compares the hash code of a file's contents with an expected value.

--- test/Test.Analyzers/Test_Crds.cs
+++ test/Test.Analyzers/Test_Crds.cs
@@ -54,6 +54,34 @@ namespace Test.Analyzers
         }
 
         [Fact]
+        public void TestIgnoreAttributeCrd()
+        {
+            using var tempFile = new TempFolder();
+
+            var testCompilation = new TestCompilationBuilder()
+                .AddSourceGenerator<CustomResourceDefinitionGenerator>()
+                .AddOption("build_property.NeonOperatorGenerateCrds", true)
+                .AddOption("build_property.NeonOperatorCrdOutputDir", tempFile.Path)
+                .AddOption("build_property.TargetDir", tempFile.Path)
+                .AddSourceFile("Models/V1ExampleEntity.cs")
+                .AddSourceFile("Models/V3ExampleEntity.cs")
+                .AddAssembly(typeof(KubernetesEntityAttribute).Assembly)
+                .AddAssembly(typeof(AdditionalPrinterColumnAttribute).Assembly)
+                .AddAssembly(typeof(V1Condition).Assembly)
+                .AddAssembly(typeof(RequiredAttribute).Assembly)
+                .Build();
+
+            var outFile = "examples.example.neonkube.io.yaml";
+
+            var output =  File.ReadAllText(Path.Combine(tempFile.Path, outFile)).GetHashCodeIgnoringWhitespace();
+
+            var expectedCrd = File.ReadAllText(Path.Combine("Outputs", outFile)).GetHashCodeIgnoringWhitespace();
+
+            output.Should().Be(expectedCrd);
+            testCompilation.Diagnostics.Should().BeEmpty();
+        }
+
+        [Fact]
         public void TestGenericCrd()
         {
             using var tempFile = new TempFolder();
